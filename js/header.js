e1e3b8ff4fff19d027f5edcc9c6d19d19c6893da
FPSyndicate <admin@fpsyndicate.com>
2017-03-12 17:19:05 -0700

    Header Updates
    Moved header to a javascript file, so we don't have to update all the
    files to change the header.
    
    Added an optional dropdown menu to the header.
    
    SEE js/header.js

---
+++ js/header.js
@@ -0,0 +1,57 @@
+
+
+
+
+var element = document.getElementById("navbar-list");
+
+var links =  ["Services", "Products", "Tour", "Features", "Testimonials", "Pricing", "Cootie Genetics!"]
+var href =  ["index.html#Services", "index.html#Products", "index.html#Tour", "index.html#Features", "index.html#Testimonials", "index.html#Pricing", "cooties.html" ]
+
+var links2 =  [[],       [],       [],    [],      [],           [],     ["Game", "Screenshots"],  ]
+var href2 =  [[],       [],       [],    [],      [],           [],     ["cooties.html#", "cooties.html#Screenshots"],  ]
+
+var str = "";
+
+for (i = 0; i < links.length; i++) { 
+
+	/*<li><a href="#" data-nav-section="services"><span>Services</span></a></li>
+	<li><a href="#" data-nav-section="products"><span>Products</span></a></li>
+	<li><a href="#" data-nav-section="tour"><span>Tour</span></a></li>
+	<li><a href="#" data-nav-section="features"><span>Features</span></a></li>
+	<li><a href="#" data-nav-section="testimonials"><span>Testimonials</span></a></li>
+	<li><a href="#" data-nav-section="pricing"><span>Pricing</span></a></li>*/
+	var li = document.createElement("li");
+	var link = document.createElement("a");
+	var span = document.createElement("span");
+	link.setAttribute("href", href[i]);
+	link.setAttribute("data-nav-section", links[i].toLowerCase());
+	link.setAttribute("class", "dropdown");
+	span.innerHTML = links[i];
+	link.appendChild(span);
+	li.appendChild(link);
+	element.appendChild(li);
+
+
+	/*
+	<div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  	</div>
+  	*/
+
+  	var drpdown = document.createElement("div");
+  	drpdown.setAttribute("class", "dropdown-content");
+  	span.appendChild(drpdown);
+
+
+  	for (j=0; j < links2[i].length; j++){
+
+  		var link2 = document.createElement("a");
+  		link2.setAttribute("href", href2[i][j]);
+  		link2.innerHTML = links2[i][j];
+  		drpdown.appendChild(link2);
+
+  	}
+    
+}
