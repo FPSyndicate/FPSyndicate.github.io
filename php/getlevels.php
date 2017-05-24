b046f667957d919970788fecc2b2e08bee07a8ed
Michael Futch <michaeltfutch@gmail.com>
2017-05-24 13:15:07 -0700

    CommunityLevels for TapDot
    Pretty much all good to go! added in voting and finalized the
    download/search one.

--- php/getlevels.php
+++ php/getlevels.php
@@ -1,15 +1,19 @@
 <?php
+	
+
+	$type = $_GET['type'];
+    $index = $_GET['index'];
+    $offset = $_GET['offset'];
+    $pw = $_GET['pw'];
 
     // Send variables for the MySQL database class.
 
-    $db = mysqli_connect('localhost', 'id1567088_tapdotadmin', 'tapdotsecretpassword') or die('Could not connect: ' . mysqli_error($db));
+    $db = mysqli_connect('localhost', 'id1567088_tapdotadmin', $pw) or die('Could not connect: ' . mysqli_error($db));
 
     mysqli_select_db($db, 'id1567088_tapdot') or die('Could not select database');
 
  
-    $type = $_GET['type'];
-    $index = $_GET['index'];
-    $offset = $_GET['offset'];
+    
 
     $num_rows = 7;
 
@@ -30,11 +34,12 @@
 		$query = "SELECT DISTINCT `LevelData` FROM `CommunityLevels` WHERE `LevelIndex` = $index;";
     }
 
+
     //$query = "SELECT DISTINCT LevelData FROM `CommunityLevels` WHERE LevelIndex='$index';";
 
     $result = mysqli_query($db, $query) or die('Query failed: ' . mysqli_error($db));
 
- 
+ 	
 
     $num_results = mysqli_num_rows($result);  
 
---
+++ php/vote.php
@@ -0,0 +1,31 @@
+<?php
+	
+
+	$type = $_GET['type'];
+    $index = $_GET['index'];
+    $pw = $_GET['pw'];
+
+    // Send variables for the MySQL database class.
+
+    $db = mysqli_connect('localhost', 'id1567088_tapdotadmin', $pw) or die('Could not connect: ' . mysqli_error($db));
+
+    mysqli_select_db($db, 'id1567088_tapdot') or die('Could not select database');
+
+    if ($type == 'upvote'){
+        $query = "UPDATE `CommunityLevels` SET `Points` = `Points` + 1 WHERE `LevelIndex` = $index;";
+    }
+    elseif ($type == 'downvote'){
+        $query = "UPDATE `CommunityLevels` SET `Points` = `Points` - 1 WHERE `LevelIndex` = $index;";
+    }
+
+
+    mysqli_query($db, $query) or die('Query failed: ' . mysqli_error($db));
+
+    if ($type == 'upvote'){
+    	echo "upvote";
+	}
+	elseif ($type == 'downvote'){
+		echo "downvote";
+	}
+
+?>
