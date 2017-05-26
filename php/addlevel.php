6a58872d253ce463a2c6a7ba71be98ab825360fe
Michael Futch <michaeltfutch@gmail.com>
2017-05-26 14:26:46 -0700

    Community Level Submission
    Wont accept any levels with the same name and description

--- php/addlevel.php
+++ php/addlevel.php
@@ -1,18 +1,29 @@
-<?php 
-    $db = mysql_connect('fpsyndicate.netne.net', 'a4491895_tapdot', 'futch2413') or die('Could not connect: ' . mysql_error()); 
-    mysql_select_db('a4491895_tapdot') or die('Could not select database');
+<?php
 
-    // Strings must be escaped to prevent SQL injection attack. 
-    $name = mysql_real_escape_string($_GET['name'], $db); 
-    $score = mysql_real_escape_string($_GET['score'], $db); 
-    $hash = $_GET['hash']; 
+    $name = addslashes ($_GET['name']);
+    $author = addslashes ($_GET['author']);
+    $desc = addslashes ($_GET['desc']);
+    $data = addslashes($_GET['data']) ;
 
-    $secretKey="mySecretKey"; # Change this value to match the value stored in the client javascript below 
+    $pw = $_GET['pw'];
 
-    $real_hash = md5($name . $score . $secretKey); 
-    if($real_hash == $hash) { 
-        // Send variables for the MySQL database class. 
-        $query = "insert into scores values (NULL, '$name', '$score');"; 
-        $result = mysql_query($query) or die('Query failed: ' . mysql_error()); 
-    } 
+    // Send variables for the MySQL database class.
+
+    $db = mysqli_connect('localhost', 'id1567088_tapdotadmin', $pw) or die('Could not connect: ' . mysqli_error($db));
+
+    mysqli_select_db($db, 'id1567088_tapdot') or die('Could not select database');
+
+    //$query = "INSERT INTO `CommunityLevels` (`LevelIndex`, `LevelName`, `LevelDescription`, `LevelData`, `LevelAuthor`, `Points`, `Reports`, `Downloads`) VALUES (NULL, '$name', '$desc', '$data', '$author', '0', '0', '0');";
+
+    $query = "INSERT `CommunityLevels` (LevelIndex, LevelName, LevelDescription, LevelData, LevelAuthor, Points, Reports, Downloads) SELECT NULL, '$name', '$desc', '$data', '$author', '0', '0', '0' WHERE NOT EXISTS (SELECT * FROM `CommunityLevels` WHERE `LevelName` = '$name' AND `LevelDescription` = '$desc'); SELECT ROW_COUNT();";
+
+    //echo $data . "<br/><br/>" . $query . "<br/><br/>";
+
+    $result = mysqli_query($db, $query) or die('Query failed: ' . mysqli_error($db));
+
+    //echo $result;
+    echo mysqli_affected_rows($db);
 ?>
+
+
+
